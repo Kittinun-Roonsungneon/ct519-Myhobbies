391d82bc9b6ba703852046aa67ace87cbebc7a2e
kittitnun <kittitnun@gmail.com>
2023-08-06 11:52:55 +0700

    update

--- myweb/connection.php
+++ myweb/connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "kittinun";
-$password = "";
+$password = "65130695";
 $database="myhobbies";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$database);
