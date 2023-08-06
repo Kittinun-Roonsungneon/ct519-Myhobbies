e07f39132482df4ee84ada7a7fee8e25bd4f5f29
kittitnun <kittitnun@gmail.com>
2023-08-06 12:38:17 +0700

    Update connection.php

--- myweb/connection.php
+++ myweb/connection.php
@@ -10,5 +10,8 @@ $conn = new mysqli($servername, $username, $password,$database);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-    
+// Set character set to utf8mb4
+if (!$conn->set_charset("utf8mb4")) {
+  die("Error setting character set: " . $conn->error);
+}   
 ?>
